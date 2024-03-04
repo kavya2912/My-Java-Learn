6cfafa3c8a1fffecf70b17e971efff9307028e44
Kavya Pandey <71292030+kavya2912@users.noreply.github.com>
2024-03-04 20:05:07 +0530

    Create Demo.java

---
+++ Patterns/Demo.java
@@ -0,0 +1,24 @@
+package Patterns;
+
+public class Demo {
+  
+
+    public static void main(String[] args) {
+        int i = 1 ;
+        int n = 5;
+        
+        
+        
+        while (i<=n) {
+          int j =1;
+        while (j <=n) {
+            System.out.print(" * ");
+            j++;  
+        }
+        System.out.println(" *");
+        i++;
+        
+            
+        }
+    }
+}

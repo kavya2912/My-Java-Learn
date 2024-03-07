6414a36f6ee074cf982061dd62aad75224cb0880
Kavya Pandey <71292030+kavya2912@users.noreply.github.com>
2024-03-07 18:18:54 +0530

    Create Main.java

---
+++ Patterns/Inverted triangle/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 1; i <=4;i++){
+            for (int j =3; i>=i;i--){
+                System.out.print(" ");
+
+            }
+            for(int k = 1; k <=i;k++){
+                System.out.println(" *");
+            }
+            System.out.println( );
+
+        }
+    }
+    
+}

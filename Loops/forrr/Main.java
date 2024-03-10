0af67671cc7b5d061aba5847416d179cc007d6db
Kavya Pandey <71292030+kavya2912@users.noreply.github.com>
2024-03-11 05:28:53 +0530

    Create Main.java

---
+++ Loops/forrr/Main.java
@@ -0,0 +1,17 @@
+package Loops.forrr;
+import java.io.*;
+public class Main {
+    
+        public static void main(String[] args) 
+            
+        {
+            int n = 5;
+            for(int i = 1; i <= 10; i++){
+                System.out.print(i * n + " ");
+            }
+            
+        }
+        
+    
+    
+}

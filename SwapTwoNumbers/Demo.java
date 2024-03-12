8ef64cfea9302c295ab589f4872b894ec613bf1c
Kavya Pandey <71292030+kavya2912@users.noreply.github.com>
2024-03-12 15:16:59 +0530

    Create Demo.java

---
+++ SwapTwoNumbers/Demo.java
@@ -0,0 +1,13 @@
+package SwapTwoNumbers;
+
+public class Demo {
+    public static void main(String[] args) {
+        int a =20, b=30,temp=0;
+        temp =a;
+        a=b;
+        b= temp;
+        System.out.println(a);
+        System.out.println(b);
+        
+        
+    }
